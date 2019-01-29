301b6838f6292b78c4b54092328077e9cce48fcd
Valentina <vale.gutierrezc@gmail.com>
2019-01-29 14:35:29 -0300

    cancancan loaded, views changed to adapt to cancancan

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  # before_action :set_post, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource
 
   # GET /posts
   # GET /posts.json
@@ -10,7 +11,7 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
-    @comments = Comment.all
+    @comments = @post.comments
   end
 
   # GET /posts/new
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Post.destroy_all
+User.destroy_all
+10.times do |i|
+  u = User.create(
+    name: "User #{i+1}",
+    email: "email#{i+1}@gmail.com",
+    password: "123456"
+
+  )
+  p = Post.create(
+    title: "Post #{i+1}",
+    content: "Contenido del post",
+    user: u
+  )
+  3.times do |j|
+    c = p.comments.build(content: "Comentario #{j+1} del post #{i+1}", user: u, post: p)
+    c.save!
+  end
+end
